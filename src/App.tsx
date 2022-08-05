44d333b34ea548f145c64e92bea8a47b422bf487
Felipe Souza Rasnheski <rasnheskisouzafelipe@gmail.com>
2022-08-05 16:19:10 -0400

    update

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,37 @@
 import { PlusCircle } from 'phosphor-react'
+import { ChangeEvent, FormEvent, useState } from 'react'
 import styles from './App.module.css'
 import Clipboard from './Assets/Clipboard.svg'
 import logo from './Assets/Logo.svg'
 import { Task } from './components/Task'
 import './Global.css'
 
+let hasContent = false
+let numberOfTasksCreated = 0
+
 export function App() {
+  const [newTask, setNewTask] = useState('')
+  const [currentTasks, setCurrentTasks] = useState([""])
+
+  function handleNewTask(event:FormEvent) {
+    event.preventDefault()
+    setCurrentTasks([...currentTasks, newTask])
+    numberOfTasksCreated++
+    setNewTask('')
+  }
+
+  function handleNewChange(event:ChangeEvent<HTMLInputElement>) {
+    setNewTask(event.target.value)
+  }
+
+  function deleteTask(content: string[]) {
+    numberOfTasksCreated--
+    setCurrentTasks(content)
+  }
+
+  const newTaskIsEmpty = newTask.length === 0
+  const tasksEmpty = currentTasks.length > 1
+
   return (
     <div>
       <header className={styles.header}>
@@ -13,30 +39,43 @@ export function App() {
       </header>
       
       <main className={styles.container}>
-        <form>
-          <input type="text" placeholder='Adicione uma nova tarefa'></input>
-          <button type="submit">Criar <span><PlusCircle size={18} /></span></button>
+        <form onSubmit={handleNewTask}>
+          <input 
+            type="text" placeholder='Adicione uma nova tarefa'
+            spellCheck={false}
+            value={newTask} onChange={handleNewChange}>
+          </input>
+          <button type="submit" disabled={newTaskIsEmpty}>Criar <span><PlusCircle size={18} /></span></button>
         </form>
 
         <div className={styles.tasks}>
           <div className={styles.headerTasks}>
-            <div className={styles.counter}>Tarefas criadas<span>0</span></div>
-            <div className={styles.counter}>Concluídas<span>0</span></div>
+            <div className={styles.counter}>Tarefas criadas<span>{numberOfTasksCreated}</span></div>
+            <div className={styles.counter}>Concluídas<span>{numberOfTasksCreated} de {}</span></div>
           </div>
-
+          
           <div className={styles.tasksBox}>
-            <div className={styles.tasksHasContent}>
+            <div className={tasksEmpty ? styles.tasksHasContent : styles.tasksEmpty}>
               <img src={Clipboard} />
               <p>Você ainda não tem tarefas cadastradas</p>
               <p><span>Crie tarefas e organize seus itens a fazer</span></p>
             </div>
-
-            <Task />
-            <Task />
-            <Task />
           </div>
         </div>
-
+        
+        
+        {currentTasks.map(task => {
+            if(task == "") return
+          return (
+            <Task 
+              key={`${numberOfTasksCreated}${task}`} 
+              test={`${numberOfTasksCreated}${task}`} 
+              content={task} 
+              currentTasks={currentTasks} 
+              deleteTask={deleteTask} 
+            />
+          )
+        })}
         
       </main>
     </div>
--- src/components/Task.tsx
+++ src/components/Task.tsx
@@ -2,11 +2,26 @@ import { Check, Trash } from 'phosphor-react';
 import { useState } from 'react';
 import styles from './Task.module.css';
 
-export function Task() {
-  const [isChecked, setChecked] = useState(false);
+interface TaskProps {
+  content: string;
+  currentTasks: string[];
+  deleteTask: (content: string[]) => void;
+  test: string;
+}
 
+export function Task({content, currentTasks, deleteTask, test}:TaskProps) {
+  const [isChecked, setChecked] = useState(false);
+  
   function handle() {
     setChecked(!isChecked)
+  }
+
+  function deletetask() {
+    const tasksWithoutDeleteOne = currentTasks.filter(task => { 
+      return task !== content
+    })
+
+    deleteTask(tasksWithoutDeleteOne)
   }
 
   return (
@@ -17,8 +32,8 @@ export function Task() {
         </div>
         <input type="checkbox" checked={isChecked} onChange={handle}/>
       </label>
-      <p className={isChecked == true ? styles.textTask : styles.test}>Lorem ipsum dolor sit amet consectetur adipisicing elit. Temporibus amet, nulla sit, veritatis ipsum laborum? Lorem ipsum dolor sit amet consectetur adipisicing elit</p>
-      <i className={styles.trash}><Trash size={20} /></i>
+      <p className={isChecked == true ? styles.textTask : styles.test}>{content}</p>
+      <i className={styles.trash} onClick={deletetask}><Trash size={20} /></i>
     </div>
   )
 }
