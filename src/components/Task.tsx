5f13eff0d844473be60d53dc08d635d664f01f51
Felipe Souza Rasnheski <rasnheskisouzafelipe@gmail.com>
2022-08-05 20:19:00 -0400

    delete task

--- src/components/Task.tsx
+++ src/components/Task.tsx
@@ -4,24 +4,20 @@ import styles from './Task.module.css';
 
 interface TaskProps {
   content: string;
-  currentTasks: string[];
-  deleteTask: (content: string[]) => void;
-  test: string;
+  deleteTask: (content: string) => void;
+  handleTaskCompleted: (content: string) => void;
 }
 
-export function Task({content, currentTasks, deleteTask, test}:TaskProps) {
-  const [isChecked, setChecked] = useState(false);
-  
-  function handle() {
-    setChecked(!isChecked)
+export function Task({content, deleteTask, handleTaskCompleted}:TaskProps) {
+  const [isChecked, setIsChecked] = useState(false)
+
+  function handleChecked() {
+    setIsChecked(!isChecked)
+    handleTaskCompleted(content)
   }
 
-  function deletetask() {
-    const tasksWithoutDeleteOne = currentTasks.filter(task => { 
-      return task !== content
-    })
-
-    deleteTask(tasksWithoutDeleteOne)
+  function removeTask() {
+    deleteTask(content)
   }
 
   return (
@@ -30,10 +26,10 @@ export function Task({content, currentTasks, deleteTask, test}:TaskProps) {
         <div className={styles.checkButton}>
           <Check className={isChecked == true ? styles.checked : styles.hidden}/>
         </div>
-        <input type="checkbox" checked={isChecked} onChange={handle}/>
+        <input type="checkbox" checked={isChecked} onChange={handleChecked}/>
       </label>
       <p className={isChecked == true ? styles.textTask : styles.test}>{content}</p>
-      <i className={styles.trash} onClick={deletetask}><Trash size={20} /></i>
+      <i className={styles.trash} onClick={removeTask}><Trash size={20} /></i>
     </div>
   )
 }
