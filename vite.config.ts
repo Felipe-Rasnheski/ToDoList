ce42e5295a181f3bd67c6112af37de8fdafb5c07
Felipe Souza Rasnheski <rasnheskisouzafelipe@gmail.com>
2022-08-20 06:32:46 -0400

    test

--- vite.config.ts
+++ vite.config.ts
@@ -3,9 +3,5 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  base: "https://Felipe-Rasnheski.github.io/ToDoList/",
-  build: {
-    outDir: 'build'
-  }
+  plugins: [react()]
 })
